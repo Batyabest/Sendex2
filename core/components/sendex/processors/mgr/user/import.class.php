3d62c150cb57a5ba8ef790d65465c95396c09583
Batyabest <batyamail@yandex.ru>
2017-05-22 11:21:21 +0300

    Исправлен баг с функционалом импорта пользователей из файла

--- core/components/sendex/processors/mgr/user/import.class.php
+++ core/components/sendex/processors/mgr/user/import.class.php
@@ -2,7 +2,7 @@
 /**
  * Create an User
  */
-class sxUserCreateProcessor extends modObjectCreateProcessor {
+class sxUserImportProcessor extends modObjectCreateProcessor {
 	public $objectType = 'sxUser';
 	public $classKey = 'sxUser';
 	public $languageTopics = array('sendex');
@@ -13,70 +13,37 @@ class sxUserCreateProcessor extends modObjectCreateProcessor {
 	 * @return bool
 	 */
 
-	/*public function process() {
+	public function process() {
 
-		print_r($this->getProperties());
-		die();
-	}*/
-
-	public function prepareQueryBeforeCount(xPDOQuery $c)
-	{
-
-		$c->innerJoin('sxUserGroup', 'sxUserGroup', 'sxUserGroup.id = sxUser.usergroup_id');
-		$c->select($this->modx->getSelectColumns('sxUser', 'sxUser'));
-		$c->select('sxUserGroup.name as group_name');
-
-		if ($usergroup_id = $this->getProperty('usergroup_id')) {
-			if (!empty($usergroup_id)) {
-				$c->where(array('sxUserGroup.usergroup_id' => $usergroup_id));
-			}
-		}
-
-		/*$query = trim($this->getProperty('query'));
-		if ($query) {
-			$c->where(array(
-				'category_fabrics:LIKE' => "%{$query}%",
-			));
-		}*/
-
-		return $c;
-	}
-
-	public function beforeSet() {
-
-
-		$path = $this->modx->getOption('base_path');
-	//	$import_source = $this->getProperty('import_source');
+$path = $this->modx->getOption('base_path');
 		$file_dir = $path . $this->getProperty('import_source'); // Путь к файлу
-
-	//	print_r($file_dir);
-	//	print_r('=======');
-
-		//die();
 
 		$file = file_get_contents($file_dir);
 		$lines = explode(PHP_EOL, $file);
 		//$array = array();
-		foreach ($lines as $key => $value) {
-			//print_r($key = $value);
+		foreach ($lines as $value) {
 			$this->setProperty('email', $value);
-			return true;
+
+			// Массив, который мы передадим в процессор, там его ловить в $scriptProperties
+			$processorProps = array(
+				'email' => $value,
+				'usergroup_id' => $this->getProperty('usergroup_id')
+			);
+			// Массив опций для метода runProcessor
+			$otherProps = array(
+				// Здесь указываем где лежат наши процессоры
+				'processors_path' => $this->modx->getOption('base_path') . 'core/components/sendex/processors/'
+			);
+			// Запускаем
+			$response = $this->modx->runProcessor('mgr/user/create', $processorProps, $otherProps);
+			//print_r($response->response);
 		}
 
-		/*$required = array('import_source', 'usergroup_id');
-		foreach ($required as $tmp) {
-			if (!$this->getProperty($tmp)) {
-				$this->addFieldError($tmp, $this->modx->lexicon('field_required'));
-			}
-		}
 
-		if ($this->hasErrors()) {
-			return false;
-		}*/
-
-		return !$this->hasErrors();
 	}
+
+
 
 }
 
-return 'sxUserCreateProcessor';
+return 'sxUserImportProcessor';
